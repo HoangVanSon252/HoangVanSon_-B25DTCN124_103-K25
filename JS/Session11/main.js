5833994aa6912c9420de31cb8d4e5816d0d9ae71
Hoang Van Son <hoangvanson2552@gmail.com>
2026-03-01 11:18:58 +0700

    Session11

--- JS/Session11/main.js
+++ JS/Session11/main.js
@@ -62,6 +62,87 @@ const updateStudentProfile = (studentsArray) => {
   studentsArray[studentIndex].name = name || studentsArray[studentIndex].name;
   studentsArray[studentIndex].gpa = gpa || studentsArray[studentIndex].gpa;
 };
+
+const deleteRecord = (studentsArray) => {
+  let id = parseInt(prompt("Enter student ID to delete:"));
+  let xacNhan;
+  const studentIndex = studentsArray.findIndex(
+    (student, index) => index === id - 1,
+  );
+  if (studentIndex === -1) {
+    alert("No student found with ID: " + id);
+    return;
+  } else {
+    xacNhan = prompt(`
+      Are you sure you want to delete?
+      ID: ${studentIndex + 1} | Name: ${studentsArray[studentIndex].name} | Age: ${studentsArray[studentIndex].age} | GPA: ${studentsArray[studentIndex].gpa} | Status: ${studentsArray[studentIndex].status}
+
+      Type "yes" to comfirm:
+      `);
+    if (xacNhan.toLowerCase() !== "yes") {
+      alert(`Student ${studentsArray[studentIndex].name} has been deleted.`);
+      return;
+    }
+  }
+  studentsArray.splice(studentIndex, 1);
+  alert("Student deleted successfully!");
+};
+
+const complianceVerification = (studentsArray) => {
+  const minors = studentsArray.filter((s) => s.age < 18);
+
+  const inactiveStudents = studentsArray.filter((s) => s.status !== "active");
+
+  const hasMinors = minors.length > 0;
+  const allActive = inactiveStudents.length === 0;
+
+  const minorNames = minors.map((s) => `${s.name} (Age: ${s.age})`).join(", ");
+  const inactiveNames = inactiveStudents
+    .map((s) => `${s.name} (Status: ${s.status})`)
+    .join(", ");
+
+  alert(`========== COMPLIANCE VERIFICATION ==========
+  Has at least one student under 18: ${hasMinors ? "Yes" : "No"}
+  Minors found:
+  -> ${hasMinors ? minorNames : "None"}
+  
+  All students have "active" status: ${allActive ? "Yes" : "No"}
+  Inactive students: 
+  -> ${!allActive ? inactiveNames : "None"}`);
+};
+const academicStatistics = (studentsArray) => {
+  if (studentsArray.length === 0) return alert("Danh sách trống!");
+
+  let totalGPA = studentsArray.reduce((sum, student) => sum + student.gpa, 0);
+  let averageGPA = (totalGPA / studentsArray.length).toFixed(2);
+
+  // Khởi tạo max/min bằng phần tử đầu tiên
+  let highestStudent = studentsArray.reduce(
+    (max, s) => (s.gpa > max.gpa ? s : max),
+    studentsArray[0],
+  );
+  let lowestStudent = studentsArray.reduce(
+    (min, s) => (s.gpa < min.gpa ? s : min),
+    studentsArray[0],
+  );
+
+  alert(`========== ACADEMIC STATISTICS ==========
+  Average GPA: ${averageGPA}
+  Highest GPA: ${highestStudent.name} (${highestStudent.gpa});
+  Lowest GPA: ${lowestStudent.name} (${lowestStudent.gpa})`);
+};
+
+const dataNormalization = (studentsArray) => {
+  let studentList = "";
+  studentsArray.forEach((student, index) => {
+    studentList += `ID: ${index + 1} | Name: ${student.name.toUpperCase()} | Age: ${student.age} | GPA: ${student.gpa} | Status: ${student.status}\n`;
+  });
+  alert(`========== DATA NORMALIZATION ==========
+    ---------------------------------------------
+    ${studentList}
+    ---------------------------------------------
+    Total: ${studentsArray.length} students`);
+};
 do {
   choice = parseInt(
     prompt(`
@@ -97,16 +178,16 @@ do {
       updateStudentProfile(students);
       break;
     case 5:
-      deleteRecord();
+      deleteRecord(students);
       break;
     case 6:
-      complianceVerification();
+      complianceVerification(students);
       break;
     case 7:
-      academicStatistics();
+      academicStatistics(students);
       break;
     case 8:
-      dataNormalization();
+      dataNormalization(students);
       break;
     case 0:
       alert("Exiting the system. Goodbye!");
